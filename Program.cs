b2d13f96f9049044b2ed272652a5b3413cc6065a
Salma Begum <Salma.Begum@medivet.co.uk>
2023-08-26 00:33:12 +0100

    Committing Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Authentication;
+
+namespace BudgetCalculator
+{
+    internal class Program
+    {
+        static void 
+            Main(string[] args)
+        {
+            //----------------Balance
+            decimal balance = 0m;
+            //Prompt user to input their starting balance
+            Console.WriteLine("Enter your starting balance:");
+            decimal startingBalance = Convert.ToDecimal(Console.ReadLine());
+
+            //----------------Income
+            //Prompt user to input income
+            Console.WriteLine("Enter your income:");
+            decimal income = Convert.ToDecimal(Console.ReadLine());
+
+            //----------------Expenses
+            decimal expenses = 0m;
+
+
+            //----------------Surplus
+
+
+
+
+
+        }
+    }
+}
